8f2cf93697f1c77b047f233b745a352216191311
Hema <157674008+Hemayuva@users.noreply.github.com>
2024-04-11 10:22:38 +0530

    Create Route_table_public.tf

---
+++ Route_table_public.tf
@@ -0,0 +1,16 @@
+# creating Route table 
+resource "aws_route_table" "route" {
+vpc_id = aws_vpc.my_vpc.id
+route {
+  cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.My igw.id
+  }
+tags = {
+  Name = " Route to internet "
+  }
+}
+
+# Associated Route table
+resource "aws_route_table_association" "rt1"
+subnet_id = aws_subnet.public_subnet_1
+route_table_id = aws_route_table.route.id
