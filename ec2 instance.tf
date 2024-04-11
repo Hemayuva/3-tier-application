d2844f46ba394f407ca87ce168ed17def7452d24
Hema <157674008+Hemayuva@users.noreply.github.com>
2024-04-11 19:35:33 +0530

    Update ec2 instance.tf

--- ec2 instance.tf
+++ ec2 instance.tf
@@ -1,13 +1,31 @@
-resource "aws_instance" "demoinstance" {
-ami  = "ami-087c17d1fe0178315"
-instance_type = "t2.micro"
-count = 1
-key_pair = "tests"
-security_group_ids = aws_security_group.demosg.id
-subnet_id = aws_subnet.public-subnet-1.id
-associate_public_ip_address = true
-user_data = file(data.sh)
-tags = {
-  Name = " my instance1 "
-    }
+resource "aws_instance" "demo-instance" {
+  ami                         = "ami-0451f2687182e0411"
+  instance_type               = "t2.micro"
+  count                       = 1
+  key_name                    = "hema-key"
+  security_groups             = [aws_security_group.demosg.id]
+  subnet_id                   = aws_subnet.public-subnet-1.id
+  associate_public_ip_address = true
+  user_data                   = "data.sh"
+  tags = {
+    Name = " my instance 1"
   }
+}
+resource "aws_key_pair" "hema" {
+  key_name   = "hema-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+}
+#creation of 2nd instance
+resource "aws_instance" "demo2instance" {
+  ami                         = "ami-0451f2687182e0411"
+  instance_type               = "t2.micro"
+  count                       = 1
+  key_name                    = "hema-key"
+  security_groups             = [aws_security_group.demosg.id]
+  subnet_id                   = aws_subnet.public-subnet-2.id
+  associate_public_ip_address = true
+  user_data                   = "data.sh"
+  tags = {
+    Name = " my instance2 "
+  }
+}
