63ae795fc3db1c6b04d473f651d0344f5f820ce0
Hema <157674008+Hemayuva@users.noreply.github.com>
2024-04-12 16:32:30 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,39 +1,42 @@
-resource "aws_lb" "my elb" {
-name = "my elb"
-internal = flase
-load_balancer_type = "application"
-security_groups=[aws_security_group.demosg.id]
-subnet_id = aws_subnet.public-subnet-1.id , aws_subnet.public-subnet-2.id
+resource "aws_lb" "my-elb" {
+  name               = "my-elb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.demosg.id]
+  subnets = [
+    aws_subnet.public-subnet-1.id,
+    aws_subnet.public-subnet-2.id
+  ]
 }
-resource "aws_lb_target_group" "target-elb" {
-name = "ALB TG"
-port= 80
-protocol ="HTTP"
-vpc_id= aws_vpc.my_vpc.id
+resource "aws_lb_target_group" "mytg" {
+  name     = "ALB-TG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.my_vpc.id
 }
 # creation of target group
 resource "aws_lb_target_group_attachment" "attachment" {
-target_group_arn=aws_lb_target_group.my elb.arn
-target_id= aws_instance.demo-instance.id
-port=80
-depends-on = [
- aws_instance.demo-instance
-]
+   count            = length(aws_instance.terra1instance)
+  target_group_arn = aws_lb_target_group.mytg.arn
+  target_id        = aws_instance.terra1instance[count.index].id
+  port             = 80
+  depends_on = [aws_instance.terra1instance]
+
 }
 resource "aws_lb_target_group_attachment" "attachment1" {
-target_group_arn=aws_lb_target_group.my elb.arn
-target_id = aws_instance.demo2instance.id
-port=80
-depends-on = [
- aws_instance.demo2instance
-]
+  count            = length(aws_instance.terra2instance)
+  target_group_arn = aws_lb_target_group.mytg.arn
+  target_id        = aws_instance.terra2instance[count.index].id
+  port             = 80
+  depends_on = [aws_instance.terra2instance]
+
 }
-resource "aws_lb_listener" "my elb" {
-load_balancer_arn=aws_lb.my elb.arn
-port=80
-protocol = "http"
-default_action {
-type = "forward"
-target_group_arn= aws_lb_target_group.my elb.arn
-}
+resource "aws_lb_listener" "my-elb" {
+  load_balancer_arn = aws_lb.my-elb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.mytg.arn
+  }
 }
