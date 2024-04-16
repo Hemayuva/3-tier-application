7aedc391dd9af0efa92549c069418065640fe8a4
Hema <157674008+Hemayuva@users.noreply.github.com>
2024-04-16 13:11:43 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,21 +1,22 @@
 # creating RDS instance
 resource "aws_db_subnet_group" "default" {
-name="main"
-subnet_ids = [aws_subnet.database-subnet-1.id,aws_subnet.database-subnet-2.id]
-tags = {
-  Name = "My db subnet group"
- }
+  name       = "main"
+  subnet_ids = [aws_subnet.Database-subnet-1.id, aws_subnet.Database-subnet-2.id]
+  tags = {
+    Name = "My-db-subnet-group"
+  }
 }
-resource "aws_db_instance" "default" {
-allocated_storage = 10
-db_subnet_group_name = aws_db_subnet_group.default.id
-engine= "MYSQL"
-engine_version ="8.0.20"
-instance_class="db.t2.micro"
-multi_az=true
-name= "mydb"
-username="username"
-password="password"
-skip_final_snapshot= true
-security_group_ids=[aws_security_group.database-sg.id]
+resource "aws_db_instance" "my-rds" {
+  allocated_storage      = 10
+  db_subnet_group_name   = aws_db_subnet_group.default.name
+  engine                 = "mysql"
+  engine_version         = "8.0.34"
+  instance_class         = "db.t3.micro"
+  multi_az               = "true"
+  identifier             = "my-rds-db"
+  username               = "Hema"
+  password               = "Hema0608#"
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.database-sg.id]
 }
+
