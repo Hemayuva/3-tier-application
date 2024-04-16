c147762b911be8ec5406346ae19612adab82a5e7
Hema <157674008+Hemayuva@users.noreply.github.com>
2024-04-16 12:35:16 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,42 +1,37 @@
-resource "aws_lb" "my-elb" {
-  name               = "my-elb"
+# creating External Load balancer
+resource "aws_lb" "LB_elb" {
+  name               = "External-LB"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.demosg.id]
-  subnets = [
-    aws_subnet.public-subnet-1.id,
-    aws_subnet.public-subnet-2.id
-  ]
+  subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
 }
-resource "aws_lb_target_group" "mytg" {
+resource "aws_lb_target_group" "target-elb" {
   name     = "ALB-TG"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.my_vpc.id
 }
-# creation of target group
 resource "aws_lb_target_group_attachment" "attachment" {
-   count            = length(aws_instance.terra1instance)
-  target_group_arn = aws_lb_target_group.mytg.arn
+  count            = length(aws_instance.terra1instance)
+  target_group_arn = aws_lb_target_group.target-elb.arn
   target_id        = aws_instance.terra1instance[count.index].id
   port             = 80
-  depends_on = [aws_instance.terra1instance]
-
+  depends_on       = [aws_instance.terra1instance]
 }
 resource "aws_lb_target_group_attachment" "attachment1" {
   count            = length(aws_instance.terra2instance)
-  target_group_arn = aws_lb_target_group.mytg.arn
+  target_group_arn = aws_lb_target_group.target-elb.arn
   target_id        = aws_instance.terra2instance[count.index].id
   port             = 80
-  depends_on = [aws_instance.terra2instance]
-
+  depends_on       = [aws_instance.terra2instance]
 }
-resource "aws_lb_listener" "my-elb" {
-  load_balancer_arn = aws_lb.my-elb.arn
+resource "aws_lb_listener" "LB_elb" {
+        load_balancer_arn = aws_lb.LB_elb.arn
   port              = 80
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.mytg.arn
+    target_group_arn = aws_lb_target_group.target-elb.arn
   }
 }
