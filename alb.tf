f27f8c9770a20f6d7f53c99c77d8ba47d3a122c1
Hema <157674008+Hemayuva@users.noreply.github.com>
2024-04-16 13:13:01 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -6,32 +6,32 @@ resource "aws_lb" "LB_elb" {
   security_groups    = [aws_security_group.demosg.id]
   subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
 }
-resource "aws_lb_target_group" "target-elb" {
-  name     = "ALB-TG"
+resource "aws_lb_target_group" "tg-elb" {
+  name     = "ALB-TG1"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.my_vpc.id
 }
 resource "aws_lb_target_group_attachment" "attachment" {
   count            = length(aws_instance.terra1instance)
-  target_group_arn = aws_lb_target_group.target-elb.arn
+  target_group_arn = aws_lb_target_group.tg-elb.arn
   target_id        = aws_instance.terra1instance[count.index].id
   port             = 80
   depends_on       = [aws_instance.terra1instance]
 }
 resource "aws_lb_target_group_attachment" "attachment1" {
   count            = length(aws_instance.terra2instance)
-  target_group_arn = aws_lb_target_group.target-elb.arn
+  target_group_arn = aws_lb_target_group.tg-elb.arn
   target_id        = aws_instance.terra2instance[count.index].id
   port             = 80
   depends_on       = [aws_instance.terra2instance]
 }
 resource "aws_lb_listener" "LB_elb" {
-        load_balancer_arn = aws_lb.LB_elb.arn
+ load_balancer_arn = aws_lb.LB_elb.arn
   port              = 80
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.target-elb.arn
+    target_group_arn = aws_lb_target_group.tg-elb.arn
   }
 }
